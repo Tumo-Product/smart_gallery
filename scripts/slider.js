4598611b396404df785e43d5683e5c8611b9ace8
vbalyan02 <vitaliy.balyan@tumo.org>
2021-05-25 17:54:42 +0400

    demo_layer_system

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,29 +1,41 @@
-var slideIndex = 1;
-// var width = 
-show_slides(slideIndex);
+// var slideIndex = 1;
 
-function plus_slide(n) {
-  show_slides(slideIndex += n);
-}
+// show_slides(slideIndex);
 
-function show_slides(n) {
-    var slides = document.getElementsByClassName("slides");
+// function plus_slide(n) {
+//   show_slides(slideIndex += n);
+// }
 
-    if (n > slides.length)
-    {
-        slideIndex = 1
-    }
+// function show_slides(n) {
+//     var slides = document.getElementsByClassName("slides");
 
-    if (n < 1) 
-    {
-        slideIndex = slides.length
-    }
+//     if (n > slides.length)
+//     {
+//         slideIndex = 1
+//     }
 
-    for (let i = 0; i < slides.length; i++) 
-    {
-        slides[i].style.display = "none";
-    }
+//     if (n < 1) 
+//     {
+//         slideIndex = slides.length
+//     }
+
+//     for (let i = 0; i < slides.length; i++) 
+//     {
+//         slides[i].style.display = "none";
+//     }
     
-    slides[slideIndex-1].style.display = "flex";
-}
+//     slides[slideIndex-1].style.display = "flex";
+// }
 
+var slider = document.getElementById("myRange");
+
+
+slider.oninput = function ()
+{ 
+    let moveable_div = document.getElementsByClassName("moveable");
+    let width = slider.value;
+    width += "%";
+    // console.log(width);
+    moveable_div[0].style.width = width;
+    // console.log(window.getComputedStyle(moveable_div[0]).width);
+}
