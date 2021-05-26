98310d235f72ca24d74ceb4a5d757b32a9a087c1
HaykAvagyan1 <hayk.avagyan@tumo.org>
2021-05-26 16:23:34 +0400

    Fully updated and new smart gallery
    New slider for the gallery. Swiping for the different image sets.

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,11 +1,11 @@
-var swiper = new Swiper('.blog-slider', {
+let swiper = new Swiper('.blog-slider', {
     spaceBetween: 30,
     effect: 'fade',
     loop: true,
+    simulateTouch: false,
     mousewheel: {
         invert: false,
     },
-    // autoHeight: true,
     pagination: {
         el: '.blog-slider__pagination',
         clickable: true,
