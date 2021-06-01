c9ba63f095826dd02675d8fe80641c036806412a
HaykAvagyan1 <hayk.avagyan@tumo.org>
2021-06-01 13:39:52 +0400

    Loading screen added, loading from axios added(just need link from David now)

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,7 +1,6 @@
 let swiper = new Swiper('.blog-slider', {
     spaceBetween: 30,
     effect: 'fade',
-    loop: true,
     simulateTouch: false,
     mousewheel: {
         invert: false,
@@ -10,4 +9,42 @@ let swiper = new Swiper('.blog-slider', {
         el: '.blog-slider__pagination',
         clickable: true,
     }
-}); 
+});
+
+let iframesLoaded = 0;
+let data = {};
+data.count = 3;
+
+let onLoad = async () =>
+{
+    let _uid = parser.getUid();
+
+    if (_uid)
+    {
+        data = await parser.dataFetch(_uid);
+
+        for (let iframe of data.iframes)
+        {
+            $(".swiper-wrapper").append(
+                `<div class="blog-slider__item swiper-slide">
+                    <iframe onload="loaded()" src="module.html?_uid=${iframe}"></iframe>
+                </div>`);
+        }
+    }
+    else
+    {
+        console.log("uid not found");
+    }
+}
+
+const loaded = () =>
+{
+    iframesLoaded++;
+
+    if (iframesLoaded == data.count)
+    {
+        loader.toggle();
+    }
+}
+
+$(onLoad)
